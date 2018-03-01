a25d2f053c3389e9cf62998a0ea4f22a2aedcfc2
Omar Cruz <omar.cruzcorrea@gmail.com>
2018-03-01 16:25:01 -0600

    La ruta para la referencia de bowtie2 se designa una unica vez en la parte de variables en vez de varias veces a lo largo del script

--- separaHumano.sh
+++ separaHumano.sh
@@ -19,6 +19,7 @@ fi
 DIRin=$1
 DIRout=$2
 Start=$(pwd)
+Ref="/home/ocruz/Public/hg19" #path y bowtie2 reference prefix
 
 #Manejo de directorios
 mkdir -p $DIRout
@@ -29,7 +30,7 @@ mkdir -p $DIRout/$h
 cd $DIRin/$h
 # separar las lecturas humanas y no humanas con bowtie2
 # modo paired end para las lecturas pareadas no extendidas con flash
-bowtie2 -x /ocruz/Publico/hg19 \
+bowtie2 -x $Ref \
 -1 $DIRin/$h/out.notCombined.filtered_1P.fastq.gz \
 -2 $DIRin/$h/out.notCombined.filtered_2P.fastq.gz \
 --un-conc-gz $DIRout/$h/notCombined_%P_nothuman.fastq.gz \
@@ -37,7 +38,7 @@ bowtie2 -x /ocruz/Publico/hg19 \
 > $DIRout/$h/notCombined_paired_human.sam
 
 # modo single read para las lecturas no pareadas no extendidas con flash
-bowtie2 -x /ocruz/Publico/hg19 \
+bowtie2 -x $Ref \
 -1 $DIRin/$h/out.notCombined.filtered_1U.fastq.gz \
 -2 $DIRin/$h/out.notCombined.filtered_2U.fastq.gz \
 --un-conc-gz $DIRout/$h/notCombined_%U_nothuman.fastq.gz \
@@ -45,13 +46,12 @@ bowtie2 -x /ocruz/Publico/hg19 \
 > $DIRout/$h/notCombined_unpaired_human.sam
 
 # modo single read para las lecturas extendidas con flash
-bowtie2 -x /ocruz/Publico/hg19 \
+bowtie2 -x $Ref \
 -U $DIRin/$h/out.extendedFrags.fastq.gz \
 --un-gz $DIRout/$h/extendedFrags_nothuman.fastq.gz \
 --al-gz $DIRout/$h/extendedFrags_human.fastq.gz \
 > $DIRout/$h/extendedFrags_human.sam
-# regresar al nivel anterior de directorios
-#cd ..
+
 done
 
 # regresar al directorio donde se corrio el script
